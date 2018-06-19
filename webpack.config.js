fa90578f4cee2a6689e8c19c4f08a558fa5bc498
Christopher Tobin <christobin@Christophers-MacBook.local>
2018-06-18 21:12:01 -0400

    configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,10 @@
-var webpack = require('webpack');
-var path = require('path');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const path = require('path');
 
 module.exports = {
-  entry: {
-    bundle: './index.js',
-    vendor: ['react', 'react-dom', 'prop-types', 'redux', 'redux-thunk']
-  },
-  output: {
-    filename: 'scripts/[name].js',
-    path: path.resolve(__dirname, 'public')
-  },
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin({
-        names: ['vendor', 'manifest'] // Specify the common bundle's name.
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.jsx?/,
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader']
-        })
-      }
-    ]
-  },
-  plugins: [
-    new ExtractTextPlugin({
-      filename:  (getPath) => {
-        return getPath('css/styles.css');
-      },
-      allChunks: true
-    })
-  ]
+    mode: "development",
+    entry: './helpers/randomizer.js',
+    output: {
+        path: path.resolve(__dirname, 'public/scripts'),
+        filename: 'helpers.js'
+    }
 };
