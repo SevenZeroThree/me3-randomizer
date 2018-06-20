72b20141523f430125f26659a3d216f021899a67
Christopher Tobin <christobin@Christophers-MacBook.local>
2018-06-19 22:35:06 -0400

    update bundle names

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'public/scripts'),
-        filename: '[name].js'
+        filename: '[name].bundle.js'
     },
 
     module: {
