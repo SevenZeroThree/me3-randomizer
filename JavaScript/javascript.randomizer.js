7519e38f9bfbb0d17e8d438de9b35456fb73dc3a
Christopher Tobin <1302813+christophertobin@users.noreply.github.com>
2018-06-20 21:32:03 -0400

    add regenerate button

--- JavaScript/javascript.randomizer.js
+++ JavaScript/javascript.randomizer.js
@@ -1,9 +1,19 @@
 import Randomizer from '../helpers/randomizer';
 
-var randomClass = Randomizer.getRandomClass();
+class JavaScriptRandomizer {
+    static generate() {
+        var randomClass = Randomizer.getRandomClass();
 
-var characterDiv = document.getElementById('character');
-characterDiv.children[0].innerHTML = randomClass.character.name;
+        var characterDiv = document.getElementById('character');
+        characterDiv.children[0].innerHTML = randomClass.character.name;
+        
+        var weaponDiv = document.getElementById('weapon');
+        weaponDiv.children[0].innerHTML = randomClass.weapon.name;
+    }
+}
 
-var weaponDiv = document.getElementById('weapon');
-weaponDiv.children[0].innerHTML = randomClass.weapon.name;
+JavaScriptRandomizer.generate();
+
+document.getElementById('regenerate-button').addEventListener('click', function() {
+    JavaScriptRandomizer.generate();
+});
