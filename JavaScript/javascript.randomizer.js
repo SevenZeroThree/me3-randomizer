de17f5cc78bab3997e97ac293634e5c6cec094ba
Christopher Tobin <1302813+christophertobin@users.noreply.github.com>
2018-06-28 22:08:58 -0400

    add initial character images

--- JavaScript/javascript.randomizer.js
+++ JavaScript/javascript.randomizer.js
@@ -5,6 +5,9 @@ function generate() {
         .then((randomClass) => {
             var characterDiv = document.getElementById('character');
             characterDiv.children[0].innerHTML = randomClass.character.name;
+
+            // img
+            characterDiv.children[1].src = '/public/images/characters/' + randomClass.character.slug + '.jpg';
             
             var weaponDiv = document.getElementById('weapon');
             weaponDiv.children[0].innerHTML = randomClass.weapon.name;
