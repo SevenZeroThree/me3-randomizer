fe34862af3f5ed153118577eefa56389fd671998
Christopher Tobin <1302813+christophertobin@users.noreply.github.com>
2018-07-02 21:24:48 -0400

    add character and weapon components

---
+++ React/Components/Character.jsx
@@ -0,0 +1,13 @@
+import React, { Component} from "react";
+
+class Character extends Component{
+  render(){
+    return(
+      <div>
+        <h1>This is a character</h1>
+      </div>
+    );
+  }
+}
+
+export default Character;
---
+++ React/Components/Weapon.jsx
@@ -0,0 +1,13 @@
+import React, { Component} from "react";
+
+class Weapon extends Component{
+  render(){
+    return(
+      <div>
+        <h1>This is a weapon</h1>
+      </div>
+    );
+  }
+}
+
+export default Weapon;
