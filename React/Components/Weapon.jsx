8b7d8a4a922f367d27d608cb0246e42e0c1855f2
Christopher Tobin <1302813+christophertobin@users.noreply.github.com>
2018-07-06 20:57:25 -0400

    convert weapon to functional component

--- React/Components/Weapon.jsx
+++ React/Components/Weapon.jsx
@@ -1,18 +1,16 @@
-import React, { Component} from "react";
+import React from "react";
 import PropTypes from 'prop-types';
 
-class Weapon extends Component{
-    render() {
-        return (
-            <div className="feature__item">
-                <div className="archive__item">
-                    <div className="archive__item-body">
-                        <h2 className="archive__item-title">{ this.props.weapon.name }</h2>
-                    </div>
+function Weapon(props) {
+    return (
+        <div className="feature__item">
+            <div className="archive__item">
+                <div className="archive__item-body">
+                    <h2 className="archive__item-title">{ props.weapon.name }</h2>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 Weapon.propTypes = {
