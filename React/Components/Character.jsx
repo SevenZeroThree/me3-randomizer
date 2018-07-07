f6ab1b7aa9587755d4785fa5e825b117a3833773
Christopher Tobin <1302813+christophertobin@users.noreply.github.com>
2018-07-06 20:56:02 -0400

    convert character to functional component

--- React/Components/Character.jsx
+++ React/Components/Character.jsx
@@ -1,18 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from 'prop-types';
 
-class Character extends Component {
-    render() {
-        return (
-            <div className="feature__item">
-                <div className="archive__item">
-                    <div className="archive__item-body">
-                        <h2 className="archive__item-title">{ this.props.character.name }</h2>
-                    </div>
+function Character(props) {
+    return (
+        <div className="feature__item">
+            <div className="archive__item">
+                <div className="archive__item-body">
+                    <h2 className="archive__item-title">{ props.character.name }</h2>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 Character.propTypes = {
