7478d6fd7f8c160add8cbdafee4e866689d3e36b
Christopher Tobin <1302813+christophertobin@users.noreply.github.com>
2018-07-08 21:17:36 -0400

    correctly return data from api call

--- helpers/randomizer.js
+++ helpers/randomizer.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 class Randomizer {
     static getRandomClass() {
-        axios.get('https://therandomizerapi.azurewebsites.net/v1/video-games/mass-effect-3/multiplayer')
+        return axios.get('https://therandomizerapi.azurewebsites.net/v1/video-games/mass-effect-3/multiplayer')
             .then((response) => {
-                return response;
+                return response.data;
             });
     }
 }
