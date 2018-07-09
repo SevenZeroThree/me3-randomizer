a55891d4eb428ec6e4d119f358fcc57326371953
Christopher Tobin <1302813+christophertobin@users.noreply.github.com>
2018-07-08 20:56:19 -0400

    integrate with randomizer api

--- helpers/randomizer.js
+++ helpers/randomizer.js
@@ -1,31 +1,11 @@
-import data from './data';
+import axios from 'axios';
 
 class Randomizer {
     static getRandomClass() {
-        var randomizer = this;
-        return data.getMultiplayerData()
+        axios.get('https://therandomizerapi.azurewebsites.net/v1/video-games/mass-effect-3/multiplayer')
             .then((response) => {
-                var weaponClass = randomizer._getRandomItemFromCollection(response.weaponClasses);
-
-                return {
-                    "character": randomizer._getRandomItemFromCollection(response.characters),
-                    "weapon": randomizer._getRandomItemFromCollection(weaponClass.weapons)
-                };
+                return response;
             });
-    }
-
-    static getRandomCharacter() {
-        return this._getRandomItemFromCollection(data.characters);
-    }
-
-    static getRandomWeapon() {
-        var weaponClass = this._getRandomItemFromCollection(data.weapons);
-        return this._getRandomItemFromCollection(weaponClass.members);
-    }
-
-    static _getRandomItemFromCollection(collection) {
-        var numberOfItems = collection.length >>> 0;
-        return collection[Math.floor(Math.random() * numberOfItems)];
     }
 }
 
